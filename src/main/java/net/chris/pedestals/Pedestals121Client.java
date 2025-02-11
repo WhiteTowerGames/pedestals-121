a4357411ecf420fe59d27182bdcf66b82449fe24
Christos Maragkos <eco23140@uom.edu.gr>
2025-02-11 14:42:54 +0200

    Initial commit

---
+++ src/main/java/net/chris/pedestals/Pedestals121Client.java
@@ -0,0 +1,11 @@
+package net.chris.pedestals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Pedestals121Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/chris/pedestals/Pedestals121DataGenerator.java
@@ -0,0 +1,11 @@
+package net.chris.pedestals;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Pedestals121DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
