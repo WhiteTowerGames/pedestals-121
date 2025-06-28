646c56957868b6b1fd882fcd8328b4584e04f3fb
WhiteTowerGames <whitetowersoftware@gmail.com>
2025-06-28 19:10:47 +0300

    Fixed particles not appearing on wax and scrape mixin

--- src/main/java/net/chris/pedestals/mixin/AxeItemMixin.java
+++ src/main/java/net/chris/pedestals/mixin/AxeItemMixin.java
@@ -12,6 +12,8 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldEvents;
+import net.minecraft.world.event.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -40,6 +42,8 @@ public class AxeItemMixin {
                 assert pedestalBlockEntity != null;
                 pedestalBlockEntity.transferAllInventories(world, pos, newState.getBlock());
                 world.playSound(null, pos, SoundEvents.ITEM_AXE_SCRAPE, SoundCategory.BLOCKS);
+                world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(context.getPlayer(), world.getBlockState(pos)));
+                world.syncWorldEvent(context.getPlayer(), WorldEvents.BLOCK_SCRAPED, pos, 0);
                 context.getStack().damage(1, context.getPlayer());
                 cir.setReturnValue(ActionResult.SUCCESS);
             } else {
@@ -53,6 +57,8 @@ public class AxeItemMixin {
             assert pedestalBlockEntity != null;
             pedestalBlockEntity.transferAllInventories(world, pos, newState.getBlock());
             world.playSound(null, pos, SoundEvents.ITEM_AXE_WAX_OFF, SoundCategory.BLOCKS);
+            world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(context.getPlayer(), world.getBlockState(pos)));
+            world.syncWorldEvent(context.getPlayer(), WorldEvents.WAX_REMOVED, pos, 0);
             context.getStack().damage(1, context.getPlayer());
             cir.setReturnValue(ActionResult.SUCCESS);
         }
--- src/main/java/net/chris/pedestals/mixin/HoneycombItemMixin.java
+++ src/main/java/net/chris/pedestals/mixin/HoneycombItemMixin.java
@@ -12,6 +12,8 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldEvents;
+import net.minecraft.world.event.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -35,6 +37,8 @@ public class HoneycombItemMixin {
                 pedestalBlockEntity.transferAllInventories(world, pos, newBlock);
                 context.getStack().decrement(1);
                 world.playSound(null, pos, SoundEvents.ITEM_HONEYCOMB_WAX_ON, SoundCategory.BLOCKS);
+                world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(context.getPlayer(), world.getBlockState(pos)));
+                world.syncWorldEvent(context.getPlayer(), WorldEvents.BLOCK_WAXED, pos, 0);
                 cir.setReturnValue(ActionResult.SUCCESS);
             }
         }
